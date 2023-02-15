85f35d40e23af1e69c9cfec913cfee1d89513158
om <90174038+omchaudhari1107@users.noreply.github.com>
2023-02-15 18:49:18 +0530

    Delete java directory

--- java/j_56_DontuseMultipleInheritance.java
+++
@@ -1,6 +0,0 @@
-public class j_56_DontuseMultipleInheritance {
-    public static void main(String[] args) {
-        //multiple inheritance can be allow in java using interface only....
-        //refer notes
-    }
-}
--- java/j_59_Polymorphism_in_Interface.java
+++
@@ -1,9 +0,0 @@
-public class j_59_Polymorphism_in_Interface {
-    public static void main(String[] args) { 
-        /*
-         * The word polymorphism means having many forms. In simple words, we can define
-         * polymorphism as the ability of a message to be displayed in more than one
-         * form.
-         */
-    }
-}
